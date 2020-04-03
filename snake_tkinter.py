acbe989b55a808d0f4ee146fb1a1c5a0d29b19c3
LetuceLizard <kaliber181@gmail.com>
2020-04-03 21:00:56 +0100

    working on fruit placement

--- snake_tkinter.py
+++ snake_tkinter.py
@@ -1,16 +1,16 @@
 #snake not oob
 import tkinter as tk
-from random import choice
+from random import randrange
 
 SIZE = 5
-INCREMENT = 13
+INCREMENT = 15
 GAME_SPEED = 200
 class snake(tk.Canvas):
 	def __init__(self, master):
 		super().__init__(width=600, height=600, highlightthickness=0, background="white")
 
 		self.snake_pos = [(125, 105, 115, 95), (120, 105, 110, 95), (105, 105, 95, 95)]
-		self.food_pos = (150, 150)
+		self.fruit_pos = (200, 180, 190, 170)
 		self.key= "l"
 		self.after(GAME_SPEED, self.game_clock)
 		self.bind_all('<Key>', self.get_key)
@@ -25,8 +25,7 @@ class snake(tk.Canvas):
 		for individual_snake_block in self.snake_pos:
 			self.create_rectangle(*individual_snake_block, tag="snake", fill="black") #x1, y1, x2, y2
 
-			self.create_rectangle(self.food_pos[0]+SIZE, self.food_pos[1]+SIZE, self.food_pos[0]-SIZE, \
-				self.food_pos[1]-SIZE, tag="food", fill="red")
+			self.create_rectangle(*self.fruit_pos, tag="fruit", fill="red")
 
 	def move_snake(self): 
 		head_x1, head_y1, head_x2, head_y2 = self.snake_pos[0]
@@ -40,14 +39,45 @@ class snake(tk.Canvas):
 		elif self.key == "k":
 			new_head = (head_x1, head_y1 + INCREMENT, head_x2, head_y2 + INCREMENT)
 
-		self.snake_pos = [new_head]	 + self.snake_pos[:-1]
+			"""Adding a block to the snake if the snake head == fruit_cordinates"""
+		if self.snake_pos[0] == self.fruit_pos: 
+			self.create_rectangle(*self.snake_pos[0], fill="black", tag="snake")
+			self.snake_pos = [new_head]	 + self.snake_pos
+
+			self.move_fruit()
+
+			"""Removing the last block and adding a new head"""
+		else:
+			self.snake_pos = [new_head]	 + self.snake_pos[:-1]
+
+
 		for current_pos, expected_pos in zip(self.find_withtag("snake"), self.snake_pos):
-			# print(current_pos, expected_pos)
 			self.coords(current_pos, *expected_pos)
 
 	def game_clock(self):
 		self.move_snake()
+		self.check_collsion()
+		# print(self.snake_pos)
 		self.after(GAME_SPEED, self.game_clock)
+
+	def check_collsion(self):
+		if self.snake_pos[0] in self.snake_pos[1:]:
+			self.quit()
+
+	def move_fruit(self):
+
+		yeet=self.move("fruit", randrange(0, 300, 30), randrange(0, 300, 30))
+		print(yeet)
+		print(self.fruit_pos)
+
+		self.fruit_pos = self.find_withtag("fruit")
+		print(self.fruit_pos)
+			
+
+
+		
+
+
 
 	def get_key(self, event):
 		if event.char in ["i", "l", "k", "j"]:
