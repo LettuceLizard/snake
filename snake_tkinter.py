71ca312290459eef89874ed92c692988eef0ea63
LetuceLizard <kaliber181@gmail.com>
2020-04-03 12:29:01 +0100

    arbetar på move snake

--- snake_tkinter.py
+++ snake_tkinter.py
@@ -1,31 +1,49 @@
 #snake not oob
 import tkinter as tk
-from random import randint
+from random import choice
 
+SIZE = 6
+INCREMENT = 10
 class snake(tk.Canvas):
 	def __init__(self, master):
 		super().__init__(width=600, height=600, highlightthickness=0)
 
-		self.snake_positions = [(100, 100), (115, 100), (130, 100)]
+		self.snake_pos = [(130, 100), (115, 100), (100, 100)]
+		self.food_pos = (150, 150)
 
-		self.draw_snake_rect()
+		self.create_objects()
+		self.move_snake()
+		
 
 		self.pack()
 
-	def draw_snake_rect(self):
-		for x_pos, y_pos in self.snake_positions:
-			size = 6
-			self.create_rectangle(x_pos+size, y_pos+size, x_pos-size, y_pos-size) #x1, y1, x2, y2
+	def create_objects(self):
+		"""(x_pos, y_pos) är mitten av kvadraten, SIZE är från mitt punkten tills kvadraten är ritad (radius)""" 
+		for x_pos, y_pos in self.snake_pos:
+			self.create_rectangle(x_pos+SIZE, y_pos+SIZE, x_pos-SIZE, y_pos-SIZE, tag="snake") #x1, y1, x2, y2
+
+			self.create_rectangle(self.food_pos[0]+SIZE, self.food_pos[1]+SIZE, self.food_pos[0]-SIZE, \
+				self.food_pos[1]-SIZE, tag="food", fill="red")
+
+	def move_snake(self):
+		head_x, head_y = self.snake_pos[0]
+
+		new_head = (head_x + INCREMENT, head_y)
+		self.snake_pos = [new_head]	 + self.snake_pos[:-1]
+		print(self.snake_pos)
+
 
 		
+
  
 
-
 def main():
+	"""Setup"""
 	root = tk.Tk()
 	root.title("snake")
 	root.tk.call("tk", "scaling", 4.0)
 	root.resizable(False, False)
+	"""Main"""
 
 	app = snake(root)
 
