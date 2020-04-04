f499512d38031a1ee120502d3e7278d8a95e9f71
LetuceLizard <kaliber181@gmail.com>
2020-04-04 13:28:12 +0100

    main app working

--- snake_tkinter.py
+++ snake_tkinter.py
@@ -1,16 +1,17 @@
-#snake not oob
 import tkinter as tk
 from random import randrange
-
-SIZE = 5
+"""Constants"""
 INCREMENT = 15
 GAME_SPEED = 200
+WIDTH = 300
+HEIGHT = 300
+
 class snake(tk.Canvas):
 	def __init__(self, master):
-		super().__init__(width=600, height=600, highlightthickness=0, background="white")
+		super().__init__(width=WIDTH, height=HEIGHT, highlightthickness=0, background="white")
 
-		self.snake_pos = [(125, 105, 115, 95), (120, 105, 110, 95), (105, 105, 95, 95)]
-		self.fruit_pos = (200, 180, 190, 170)
+		self.snake_pos = [self.random_square(), (0, 0, 0, 0) , (0, 0, 0, 0)]
+		self.fruit_pos = self.random_square()
 		self.key= "l"
 		self.after(GAME_SPEED, self.game_clock)
 		self.bind_all('<Key>', self.get_key)
@@ -21,7 +22,6 @@ class snake(tk.Canvas):
 		self.pack()
 
 	def create_objects(self):
-		"""(x_pos, y_pos) är mitten av kvadraten, SIZE är från mitt punkten tills kvadraten är ritad (radius)""" 
 		for individual_snake_block in self.snake_pos:
 			self.create_rectangle(*individual_snake_block, tag="snake", fill="black") #x1, y1, x2, y2
 
@@ -30,17 +30,14 @@ class snake(tk.Canvas):
 	def move_snake(self): 
 		head_x1, head_y1, head_x2, head_y2 = self.snake_pos[0]
 
-		if self.key == "l":
-			new_head = (head_x1 + INCREMENT, head_y1, head_x2 + INCREMENT, head_y2)
-		elif self.key == "i":
-			new_head = (head_x1, head_y1 - INCREMENT , head_x2, head_y2 - INCREMENT)
-		elif self.key == "j":
-			new_head = (head_x1 - INCREMENT, head_y1, head_x2 - INCREMENT, head_y2)
-		elif self.key == "k":
-			new_head = (head_x1, head_y1 + INCREMENT, head_x2, head_y2 + INCREMENT)
+		if self.key == "l":   new_head = (head_x1 + INCREMENT, head_y1, head_x2 + INCREMENT, head_y2)
+		elif self.key == "i": new_head = (head_x1, head_y1 - INCREMENT, head_x2, head_y2 - INCREMENT)
+		elif self.key == "j": new_head = (head_x1 - INCREMENT, head_y1, head_x2 - INCREMENT, head_y2)
+		elif self.key == "k": new_head = (head_x1, head_y1 + INCREMENT, head_x2, head_y2 + INCREMENT)
 
-			"""Adding a block to the snake if the snake head == fruit_cordinates"""
-		if self.snake_pos[0] == self.fruit_pos: 
+		"""Adding a block to the snake if the snake head == fruit_cordinates"""
+		if self.snake_pos[0] == self.fruit_pos:
+			print(self.snake_pos[0], self.fruit_pos) 
 			self.create_rectangle(*self.snake_pos[0], fill="black", tag="snake")
 			self.snake_pos = [new_head]	 + self.snake_pos
 
@@ -63,34 +60,21 @@ class snake(tk.Canvas):
 	def check_collsion(self):
 		if self.snake_pos[0] in self.snake_pos[1:]:
 			self.quit()
-
-	def move_fruit(self):
-
-		yeet=self.move("fruit", randrange(0, 300, 30), randrange(0, 300, 30))
-		print(yeet)
-		print(self.fruit_pos)
-
-		self.fruit_pos = self.find_withtag("fruit")
-		print(self.fruit_pos)
-			
-
-
 		
 
+	def move_fruit(self):
+		self.delete("fruit")
+		self.fruit_pos = self.random_square()
+		self.create_rectangle(*self.fruit_pos, fill="red", tag="fruit")
+
+	def random_square(self):
+		random_location = randrange(30, WIDTH, 30) #min size, screen size, step size
+		return random_location-10, random_location-30, random_location, random_location-20
 
 
 	def get_key(self, event):
 		if event.char in ["i", "l", "k", "j"]:
 			self.key = event.char
-		# print(self.key)
-
-
-
-	# def convert_format(self, all_cordinates):
-	# 	for x, y in all_cordinates:
-
-	# 	return x+SIZE, y+SIZE, x-SIZE, y-SIZE
-
 
 def main():
 	"""Setup"""
